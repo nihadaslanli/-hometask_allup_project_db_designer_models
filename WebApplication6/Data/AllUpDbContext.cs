eca689e297f34739a5c4bc1f202e48baf432632d
nihadaslanli <nihadsasl@code.edu.az>
2024-07-30 19:43:20 +0400

    added

---
+++ WebApplication6/Data/AllUpDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication6.Models;
+
+namespace WebApplication6.Data
+{
+    public class AllUpDbContext : DbContext
+    {
+        public DbSet <Settings> Settings { get; set; }
+        public DbSet<Sliders> Sliders { get; set; }
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Brand>Brands { get; set; }
+        public DbSet<Product> products { get; set; }
+
+        public AllUpDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ WebApplication6/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication6.Models
+{
+    public class Category : BaseEntity
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name{ get; set; }
+        public string Image { get; set; }
+        public int? ParentId { get; set; }
+        public bool IsMain{ get; set; }
+        public Category Parent { get; set; }
+        public List<Category> Children { get; set; }
+
+    }
+}
